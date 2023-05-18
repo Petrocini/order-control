297739e3437adafa4a7aff3c1210e09b652105de
Petrocini <lucaspetrocinireis@hotmail.com>
2023-05-18 20:06:47 -0300

    User entity and resource

---
+++ src/main/java/com/petrocini/ordercontrol/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.petrocini.ordercontrol.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.petrocini.ordercontrol.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping 
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999", "123");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
