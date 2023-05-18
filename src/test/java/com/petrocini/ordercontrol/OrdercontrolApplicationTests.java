652b4b17e8bb57f8975a134da5d020ab50420460
Petrocini <lucaspetrocinireis@hotmail.com>
2023-05-18 19:47:10 -0300

    Project created

---
+++ src/test/java/com/petrocini/ordercontrol/OrdercontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petrocini.ordercontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdercontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
