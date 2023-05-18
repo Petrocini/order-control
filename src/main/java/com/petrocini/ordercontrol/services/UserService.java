9ed365dc06650e6e7a97054c698c87d5cda32a99
Petrocini <lucaspetrocinireis@hotmail.com>
2023-05-18 20:29:54 -0300

    Service layer, component registration

---
+++ src/main/java/com/petrocini/ordercontrol/services/UserService.java
@@ -0,0 +1,26 @@
+package com.petrocini.ordercontrol.services;
+
+import com.petrocini.ordercontrol.entities.User;
+import com.petrocini.ordercontrol.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user = repository.findById(id);
+        return user.get();
+    }
+
+}
