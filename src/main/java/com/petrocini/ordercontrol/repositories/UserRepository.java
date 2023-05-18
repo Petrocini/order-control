3bead8d0612dd1c4b548d0cb07199ee27b2c282a
Petrocini <lucaspetrocinireis@hotmail.com>
2023-05-18 20:19:35 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/petrocini/ordercontrol/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.petrocini.ordercontrol.repositories;
+
+import com.petrocini.ordercontrol.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
