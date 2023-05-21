59379b3129119bae497b82db00901128930a5240
Petrocini <lucaspetrocinireis@hotmail.com>
2023-05-21 09:03:30 -0300

    Exception handling - update

--- src/main/java/com/petrocini/ordercontrol/services/UserService.java
+++ src/main/java/com/petrocini/ordercontrol/services/UserService.java
@@ -4,6 +4,7 @@ import com.petrocini.ordercontrol.entities.User;
 import com.petrocini.ordercontrol.repositories.UserRepository;
 import com.petrocini.ordercontrol.services.exceptions.DatabaseException;
 import com.petrocini.ordercontrol.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
