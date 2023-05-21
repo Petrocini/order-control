9a50d4267f7d9da9e6971169414b57e2d2997418
Petrocini <lucaspetrocinireis@hotmail.com>
2023-05-21 09:02:13 -0300

    Exception handling - delete

--- src/main/java/com/petrocini/ordercontrol/services/UserService.java
+++ src/main/java/com/petrocini/ordercontrol/services/UserService.java
@@ -2,8 +2,11 @@ package com.petrocini.ordercontrol.services;
 
 import com.petrocini.ordercontrol.entities.User;
 import com.petrocini.ordercontrol.repositories.UserRepository;
+import com.petrocini.ordercontrol.services.exceptions.DatabaseException;
 import com.petrocini.ordercontrol.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User user) {
