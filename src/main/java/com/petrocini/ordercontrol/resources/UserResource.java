1a881510bbee3ad77bd5bff78ff6c2446f7baa08
Petrocini <lucaspetrocinireis@hotmail.com>
2023-05-21 08:40:14 -0300

    User insert

--- src/main/java/com/petrocini/ordercontrol/resources/UserResource.java
+++ src/main/java/com/petrocini/ordercontrol/resources/UserResource.java
@@ -3,13 +3,12 @@ package com.petrocini.ordercontrol.resources;
 import com.petrocini.ordercontrol.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.petrocini.ordercontrol.entities.User;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,6 +28,13 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+		User userInsert = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userInsert.getId()).toUri();
+		return ResponseEntity.created(uri).body(userInsert);
 	}
 
 }
