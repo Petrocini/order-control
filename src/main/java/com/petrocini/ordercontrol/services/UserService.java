74e40672cffe5e85f819121da623bed34f658292
Petrocini <lucaspetrocinireis@hotmail.com>
2023-05-21 08:54:59 -0300

    Exception handling - findById

--- src/main/java/com/petrocini/ordercontrol/services/UserService.java
+++ src/main/java/com/petrocini/ordercontrol/services/UserService.java
@@ -2,6 +2,7 @@ package com.petrocini.ordercontrol.services;
 
 import com.petrocini.ordercontrol.entities.User;
 import com.petrocini.ordercontrol.repositories.UserRepository;
+import com.petrocini.ordercontrol.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = repository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
